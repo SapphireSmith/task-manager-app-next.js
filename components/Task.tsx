923ddca2544d3721d164b3001a2a5ee4cdbfc4bd
Sapphire Smith <ssapphire527@gmail.com>
2024-09-21 11:15:17 +0530

    font update

--- components/Task.tsx
+++ components/Task.tsx
@@ -5,14 +5,13 @@ type PropType = {
 }
 
 const Task = ({ index }: PropType) => {
-  console.log(index);
-  
+
   return (
     <div key={index} className='w-ful p-2 rounded-md border-2 h-auto'>
       <p className='font-'>Some work to do</p>
       <div className='flex gap-2 justify-end italic text-sm font-medium text-gray-400'>
-        <p>Yesterday</p>
-        <p>13:24</p>
+        <p className='font-serif'>Yesterday</p>
+        <p className='font-serif'>13:24</p>
       </div>
     </div>
   )
